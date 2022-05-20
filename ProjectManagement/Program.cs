8d064154871c71fedf711564f1f0bec5ef1125b5
John McGaughey <JMcGaughey@tql.com>
2022-05-20 15:01:54 -0400

    Added Method to ResourcesController

--- ProjectManagement/Program.cs
+++ ProjectManagement/Program.cs
@@ -1,8 +1,15 @@
-
-
-
 using ProjectManagementLib.Models;
+using ProjectManagementLib.Controllers;
+using ProjectManagementLib;
 
 void Main() { }
 
 var db = new AppDbContext();
+
+var worksCtrl = new WorksController();
+
+var work = new Work() {
+    Id = 0, ProjectId = 1, Description = "Depression", Hours = 100, ResourceId = 9
+};
+
+worksCtrl.AddWork(work);
