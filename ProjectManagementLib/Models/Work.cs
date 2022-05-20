1d3ed8a237eadbb9e687f3d5963d0823e8a89e86
Evan Mercurio <EMercurio@tql.com>
2022-05-20 11:14:14 -0400

    added virtual instances and db

--- ProjectManagementLib/Models/Work.cs
+++ ProjectManagementLib/Models/Work.cs
@@ -5,14 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjectManagementLib {
-    internal class Work {
+    public class Work {
         public int Id { get; set; }
         public int ProjectId { get; set; }
         public int ResourceId { get; set; }
         public string Description { get; set; }
         public int Hours { get; set; } = 0;
 
-        public virtual Project Project { get; set; }
+        public virtual Project? Project { get; set; }
+        
 
     }
 }
