25ba32a634c4e74303c31b0320388f98bf6a638d
John McGaughey <JMcGaughey@tql.com>
2022-05-20 10:21:46 -0400

    pushed Resources

--- ProjectManagement/Program.cs
+++ ProjectManagement/Program.cs
@@ -1,3 +1,6 @@
-﻿
+﻿using ProjectManagementLib;
 
 // hello
+
+
+
---
+++ ProjectManagementLib/Models/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementLib.Models {
+    public class AppDbContext : DbContext{
+
+        public virtual DbSet<Resources> Resources { get; set; } = null!;
+
+
+        // constructors
+        public AppDbContext() { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        // sql connection
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            if (!optionsBuilder.IsConfigured) {
+                var connStr = @"server=localhost\sqlexpress;"
+                    + "database = ProjectManagementDb;"
+                    + "trustServerCertificate = true;"
+                    + "trusted_connection = true;";
+                optionsBuilder.UseSqlServer(connStr);
+            }
+        }
+
+
+    }
+}
